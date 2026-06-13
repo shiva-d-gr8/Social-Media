abde384963e5c366cc514cd6efaff3396b8b2604
Shiva <shiva9211@gmail.com>
2026-06-14 03:22:33 +0530

    Add router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,31 @@ import CreatePost, { createPostAction } from './components/CreatePost.jsx';
 import PostList from './components/PostList.jsx';
 import {postLoader} from './components/PostList.jsx';
 
-const router=createBrowserRouter([
-  {path: "/",
-    element:<App/>,
-     children:[
-        {path: "/",element:<PostList/>, loader:postLoader, 
-          hydrateFallbackElement:<div />
-},
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <PostList />,
+          loader: postLoader,
+          hydrateFallbackElement: <div />
+        },
+        {
+          path: "/create-post",
+          element: <CreatePost />,
+          action: createPostAction
+        }
+      ]
+    }
+  ],
+  {
+    basename: "/Social-Media"
+  }
+);
 
-        {path: "/create-post",element:<CreatePost/>, action: createPostAction}
-  ] 
-},
-  ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
